7275c54845d2b4fe9ed15cbee79b11b76b8a1979
Jacinto488 <jacintov4@gmail.com>
2025-08-26 16:10:38 +0100

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-
-//render the App component to the DOM
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
